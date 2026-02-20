9bdf5f0fd424e377b379515b2ed520395c7e5819
mlanzillotta97 <marcolanzillotta3@gmail.com>
2026-02-20 09:32:05 +0100

    Marco
    - array gusti
    - array prezzi
    - stampa menu

--- Esercizio/Program.cs
+++ Esercizio/Program.cs
@@ -2,8 +2,24 @@
 
 class Program
 {
-    static void Main()
+    static string[] gusti = { "Cioccolato", "Vaniglia", "Fragola", "Pistacchio", "Limone" };
+    static double[] prezzi = { 1,50, 1,20, 1,30, 2,00, 1,20 };
+
+    static void StampaMenu()
+    {
+        Console.WriteLine("===== BENVENUTI IN GELATERIA =====");
+        for (int i = 0; i < gusti.Length; i++)
+        {
+            Console.WriteLine($"{i} - {gusti[i]} : {prezzi[i]:0.00} € a pallina");
+        }
+        Console.WriteLine("====================================");
+    }
+
+        static void Main()
     {
         
     }
+
+
+
 }
